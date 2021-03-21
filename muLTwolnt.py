31ac16fae2f83a86cfa0c65fdeb132a80349d8b8
siwani <odhursiwani@gmail.com>
2021-03-21 16:40:54 +0400

    [tag 1.0]

--- muLTwolnt.py
+++ muLTwolnt.py
@@ -8,23 +8,23 @@ def main():
     i =(len(sys.argv)-1)
     print("le nombre d'arguments : ",i)
 
-  if(i==0):
-    n1=int(input("inserez le premier argument : "))
-    n2=int(input("inserez le deuxieme argument : "))
-    x=int(n1)
-    y=int(n2)
-    print(mul_num(x,y))
-  elif(i==1):
-       n1=int(input("inserez le premier argument : "))
-       x=int(sys.argv[1])
-       y=int(n1)
-       print(mul_num(x,y))
-  elif(i==2):
-        x=int(sys.argv[1])
-        y=int(sys.arg[2])
-        print(mul_num(x,y))
+  if (i==0):
+         n1=int(input("inserez le premier argument : "))
+         n2=int(input("inserez le deuxieme argument : "))
+         x=int(n1)
+         y=int(n2)
+         print(mul_num(x,y))
+  elif (i==1):
+         n1=int(input("inserez le premier argument : "))
+         x=int(sys.argv[1])
+         y=int(n1)
+         print(mul_num(x,y))
+  elif (i==2):
+         x=int(sys.argv[1])
+         y=int(sys.arg[2])
+         print(mul_num(x,y))
   else:
-       print("erreur:veuillez inserer que deux arguments")
+         print("erreur : veuillez inserer que deux arguments")
 
 main()
 
