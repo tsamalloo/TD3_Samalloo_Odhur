7d710d8b790f4158da211d99c8d984763b418238
tsamalloo <swathisamalloo@hotmail.com>
2021-03-21 12:01:19 +0400

    [0.3add]Arriver a l'etape 14

--- addTwolnt.py
+++ addTwolnt.py
@@ -1,6 +1,6 @@
 def add(a,b):
-	sum=a+b;
-	return sum;
+    sum=a+b;
+    return sum;
 
 def main():
     import sys
@@ -8,11 +8,22 @@ def main():
     i=(len(sys.argv)-1)
     print("le nombre d'arguments : ",i)
 
-    if (i==2):
-          x = int (sys.argv[1])
-          y = int (sys.argv[2])
-          print(add(x,y))
+    if (i==0):
+           n1=int(input("Inserez le premier argument : "))
+           n2=int(input("Inserez le deuxieme argument : "))
+           x=int(n1)
+           y=int(n2)
+           print(add(x,y))
+    elif (i==1):
+	    n1=int(input("Inserez le deuxieme argument : "))
+	    x=int(sys.argv[1])
+	    y=int(n1)
+	    print(add(x,y))
+    elif (i==2):
+	    x=int(sys.argv[1])
+	    y=int(sys.argv[2])
+	    print(add(x,y))
     else:
-          print("erreur : veuillez inserer que deux arguments")
+            print("erreur : veuillez inserer que deux arguments")
 
 main()
