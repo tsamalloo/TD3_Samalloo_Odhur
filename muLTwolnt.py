a9c103730ae5f5533a31cc426be2cdfe34311c3a
siwani <odhursiwani@gmail.com>
2021-03-21 12:01:02 +0400

    [mull-0.1] Arriver a l'etape 9

--- muLTwolnt.py
+++ muLTwolnt.py
@@ -2,11 +2,11 @@ def mul(a,b):
   mul=a*b;
   return mul;
 
-def main();
+def main():
     import sys
     print(sys.argv)
     x=int(sys.argv[1])
-    y=int(sys.argv[2]}
+    y=int(sys.argv[2])
     print(mul(x,y))
 main()
 
