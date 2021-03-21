997936afee9515f9564a2114b35bc559c83531d3
tsamalloo <80746753+tsamalloo@users.noreply.github.com>
2021-03-21 17:54:05 +0400

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ elif  reponse=="non":
       print("la reponse est non.okay!")
 else:
       print(" la reponse n'est ni oui ou non !!")
+      
+
+reponse =str(input("voulez-vous additionner deux int? : "))
+
+if reponse=="oui":
+   from addTwolnt import *
+elif  reponse=="non":
+      print("la reponse est non.okay!")
+else:
+      print(" la reponse n'est ni oui ou non !!")
