82315f8a89ccc71b9f652c2bdea9a35fb12ef112
siwani <odhursiwani@gmail.com>
2021-03-21 16:13:12 +0400

    [integration] creation du main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+reponse =str(input("voulez-vous multiplier deux int? : "))
+
+if reponse=="oui":
+   from muLTwolnt import *
+elif  reponse=="non":
+      print("la reponse est non.okay!")
+else:
+      print(" la reponse n'est ni oui ou non !!")
